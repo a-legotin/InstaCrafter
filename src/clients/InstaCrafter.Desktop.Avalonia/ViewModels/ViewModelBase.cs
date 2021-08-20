87f0dda6585ae812426b5d845303750446836b91
Alexander Legotin <mrproz@yandex.ru>
2021-08-20 21:12:50 +0300

    Added avalonia basic

---
+++ src/clients/InstaCrafter.Desktop.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace InstaCrafter.Desktop.Avalonia.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
