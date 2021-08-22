ddb928aedbc8fc060480dfd6cc642f50ba108609
Alexander Legotin <mrproz@yandex.ru>
2021-08-22 20:29:40 +0300

    Added api and react basic

---
+++ src/services/InstaCrafter.API/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "InstaCrafter.API", Version = "v1" }); });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InstaCrafter.API v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
