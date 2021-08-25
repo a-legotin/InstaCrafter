48cfd0dd280323284e45e502bb5a9c1966fd18f3
Alexander Legotin <mrproz@yandex.ru>
2021-08-25 18:35:57 +0300

    Changed react to typescript

--- src/clients/react-app/src/app/App.tsx
+++ src/clients/react-app/src/app/App.tsx
@@ -1,4 +1,5 @@
-import logo from '../assets/logo.svg';
+import React from 'react';
+import logo from '../logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/services/InstaCrafter.API/Controllers/WeatherForecastController.cs
+++ src/services/InstaCrafter.API/Controllers/WeatherForecastController.cs
@@ -10,7 +10,6 @@ namespace InstaCrafter.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries =
