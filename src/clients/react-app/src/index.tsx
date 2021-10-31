c5410fc9fb1a90ca4dfa5a9a976863f952c4672c
alexander <mrproz@yandex.ru>
2021-10-31 14:16:35 +0300

    Added react and docker build

--- src/clients/react-app/src/index.tsx
+++ src/clients/react-app/src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './common/App';
+import { BrowserRouter } from "react-router-dom";
+
+import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
